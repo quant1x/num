f108aa038044993f643bc9ea379e5df6c9e038c4
wangfeng <wangfengxy@sina.cn>
2024-02-12 17:49:27 +0800

    调整z-table常量名

--- ztable.go
+++ ztable.go
@@ -3,34 +3,34 @@ package num
 import "sort"
 
 const (
-	kMaximumPercentage = float64(0.9999) // 最大百分比
-	kMinimumPercentage = float64(0.0001) // 最小百分比
-	kMaxScale          = 10000           // 最大尺寸
+	ztableMaximumPercentage = float64(0.9999) // 最大百分比
+	ztableMinimumPercentage = float64(0.0001) // 最小百分比
+	ztableMaxScale          = 10000           // 最大尺寸
 )
 
 func FindPercent(zScore float64) (percent float64) {
 	index := sort.SearchFloat64s(__percentToZscore, zScore)
-	percent = float64(index) / float64(kMaxScale)
+	percent = float64(index) / float64(ztableMaxScale)
 	return percent
 }
 
 func FindZScore(percent float64) (zScore float64) {
 	// 第一步约束 percentage在0~9999范围内
-	index := int(percent*(kMaxScale)) % kMaxScale
+	index := int(percent*(ztableMaxScale)) % ztableMaxScale
 	return __percentToZscore[index]
 }
 
 // ConfidenceIntervalToZscore 通过置信区间百分比查找Z分值
 func ConfidenceIntervalToZscore(confidenceInterval float64) (zScore float64) {
 	// 约束 percentage在0~9999范围内
-	index := int(confidenceInterval*(kMaxScale)) % kMaxScale
+	index := int(confidenceInterval*(ztableMaxScale)) % ztableMaxScale
 	return __z_table[index]
 }
 
 // ZscoreToConfidenceInterval 通过分值查找置信区间
 func ZscoreToConfidenceInterval(zScore float64) (confidenceInterval float64) {
 	index := __SearchFloat64s(__z_table, zScore)
-	confidenceInterval = float64(index) / float64(kMaxScale)
+	confidenceInterval = float64(index) / float64(ztableMaxScale)
 	return confidenceInterval
 }
 
@@ -42,7 +42,7 @@ func __SearchFloat64s_v1(a []float64, x float64) int {
 func __SearchFloat64s(a []float64, x float64) int {
 	n, found := sort.Find(len(a), func(i int) int {
 		m := x - a[i]
-		return int(m * kMaxScale)
+		return int(m * ztableMaxScale)
 	})
 	if !found {
 		n = n - 1
