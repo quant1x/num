5e9a37b4e7f4d1572330dff92fbdd861ef223db8
wangfeng <wangfengxy@sina.cn>
2024-02-23 13:26:22 +0800

    shift优化及增加基准测试

--- shift.go
+++ shift.go
@@ -1,17 +1,17 @@
 package num
 
 import (
-	"math"
 	"slices"
 )
 
 // Shift 使用可选的时间频率按所需的周期数移动索引
 //
-//	N 支持前后移动
+//	N 支持前后移动, N>0 向右, N<0 向左
 func Shift[E BaseType](S []E, N any) []E {
-	return v1Shift[E](S, N)
+	return v3Shift[E](S, N)
 }
 
+// N为常量常量, 未加速处理, 且做了一次对齐操作
 func v1Shift[T BaseType](S []T, N any) []T {
 	length := len(S)
 	var _n []DType
@@ -52,38 +52,138 @@ func v1Shift[T BaseType](S []T, N any) []T {
 	return d
 }
 
-// ShiftN series切片, 使用可选的时间频率按所需的周期数移动索引
-// Deprecated: 不推荐使用
-func ShiftN[T BaseType](S []T, periods int) []T {
+// N为常量常量, 未加速处理
+func v2Shift[E BaseType](S []E, N any) []E {
+	length := len(S)
+	w := Any2Window[DType](N)
 	d := slices.Clone(S)
-	if periods == 0 {
-		return d
-	}
-
-	values := d
-	var (
-		naVals []T
-		dst    []T
-		src    []T
-	)
-
-	if shlen := int(math.Abs(float64(periods))); shlen < len(values) {
-		if periods > 0 {
-			naVals = values[:shlen]
-			dst = values[shlen:]
-			src = values
-		} else {
-			naVals = values[len(values)-shlen:]
-			dst = values[:len(values)-shlen]
-			src = values[shlen:]
+	for i, _ := range S {
+		x := w.At(i)
+		pos := int(x)
+		if DTypeIsNaN(x) || i-pos >= length || i-pos < 0 {
+			d[i] = TypeDefault[E]()
+			continue
 		}
-		copy(dst, src)
-	} else {
-		naVals = values
+		d[i] = S[i-pos]
 	}
-	for i := range naVals {
-		naVals[i] = TypeDefault[T]()
-	}
-	_ = naVals
 	return d
 }
+
+// N为常量常量, copy处理
+func v3Shift[E BaseType](S []E, N any) []E {
+	length := len(S)
+	w := Any2Window[DType](N)
+	defaultValue := TypeDefault[E]()
+	d := make([]E, length)
+	if w.IsConst() {
+		// 如果参数是常量
+		n := int(w.C)
+		m := AbsInt(n)
+		if m >= length {
+			// 如果n大于S长度, 则返回空
+			RepeatInto(d, defaultValue, length)
+			return d
+		}
+		// 分两段操作
+		if n > 0 {
+			// 左边留空
+			// 1. copy从n开始的全部数据到目标切片的n
+			copy(d[m:length], S[:length-m])
+			// 2. 前部0~n赋予默认值
+			RepeatInto[E](d[0:m], defaultValue, m)
+		} else if n < 0 {
+			// 右边留空
+			// 1. copy从0开始的全部数据到目标切片的length-m
+			copy(d[0:length-m], S[m:length])
+			// 2. 前部length-m~length赋予默认值
+			RepeatInto[E](d[length-m:length], defaultValue, m)
+		}
+		return d
+	}
+	for i, _ := range S {
+		x := w.At(i)
+		pos := int(x)
+		if DTypeIsNaN(x) || i-pos >= length || i-pos < 0 {
+			d[i] = defaultValue
+			continue
+		}
+		d[i] = S[i-pos]
+	}
+	return d
+}
+
+// N为常量常量, copy处理, v3的简化版, 可读性不高
+func v4Shift[E BaseType](S []E, N any) []E {
+	length := len(S)
+	w := Any2Window[DType](N)
+	defaultValue := TypeDefault[E]()
+	d := make([]E, length)
+	if w.IsConst() {
+		// 如果参数是常量
+		n := int(w.C)
+		m := AbsInt(n)
+		if m >= length {
+			// 如果n大于S长度, 则返回空
+			RepeatInto(d, defaultValue, length)
+			return d
+		}
+		pos := 0
+		fixed := 0
+		if n < 0 {
+			pos = m
+			fixed = length - m
+		}
+		// 分两段操作
+		// 1. copy源切片到目标切片
+		copy(d[m-pos:length-pos], S[pos:pos+length-m])
+		// 2. 空余位置填充默认值
+		RepeatInto[E](d[fixed:fixed+m], defaultValue, m)
+		return d
+	}
+	for i, _ := range S {
+		x := w.At(i)
+		pos := int(x)
+		if DTypeIsNaN(x) || i-pos >= length || i-pos < 0 {
+			d[i] = defaultValue
+			continue
+		}
+		d[i] = S[i-pos]
+	}
+	return d
+}
+
+//// ShiftN series切片, 使用可选的时间频率按所需的周期数移动索引
+//// Deprecated: 不推荐使用
+//func ShiftN[T BaseType](S []T, periods int) []T {
+//	d := slices.Clone(S)
+//	if periods == 0 {
+//		return d
+//	}
+//
+//	values := d
+//	var (
+//		naVals []T
+//		dst    []T
+//		src    []T
+//	)
+//
+//	if shlen := int(math.Abs(float64(periods))); shlen < len(values) {
+//		if periods > 0 {
+//			naVals = values[:shlen]
+//			dst = values[shlen:]
+//			src = values
+//		} else {
+//			naVals = values[len(values)-shlen:]
+//			dst = values[:len(values)-shlen]
+//			src = values[shlen:]
+//		}
+//		copy(dst, src)
+//	} else {
+//		naVals = values
+//	}
+//	for i := range naVals {
+//		naVals[i] = TypeDefault[T]()
+//	}
+//	_ = naVals
+//	return d
+//}
---
+++ shift_test.go
@@ -0,0 +1,169 @@
+package num
+
+import (
+	"gitee.com/quant1x/num/labs"
+	"slices"
+	"testing"
+)
+
+func TestShift(t *testing.T) {
+	type args struct {
+		x any
+		y any
+	}
+	type testCase struct {
+		Name     string
+		Args     args
+		Want     any
+		TestFunc func(v any) any
+	}
+	tests := []testCase{
+		{
+			Name: "shift-left-1",
+			Args: args{
+				x: []int32{-1, 1, -2, -3},
+				y: 1,
+			},
+			Want: []int32{0, -1, 1, -2},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return Shift(args.x.([]int32), args.y.(int))
+			},
+		},
+		{
+			Name: "int32-right-1",
+			Args: args{
+				x: []int32{-1, 1, -2, -3, 1, 5},
+				y: -1,
+			},
+			Want: []int32{1, -2, -3, 1, 5, 0},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return Shift(args.x.([]int32), args.y.(int))
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			if got := tt.TestFunc(tt.Args); !labs.DeepEqual(got, tt.Want) {
+				t.Errorf("Shift() = %v, want %v", got, tt.Want)
+			}
+		})
+	}
+}
+
+func TestShiftV4(t *testing.T) {
+	type args struct {
+		x any
+		y any
+	}
+	type testCase struct {
+		Name     string
+		Args     args
+		Want     any
+		TestFunc func(v any) any
+	}
+	tests := []testCase{
+		{
+			Name: "shift-left-1",
+			Args: args{
+				x: []int32{-1, 1, -2, -3},
+				y: 1,
+			},
+			Want: []int32{0, -1, 1, -2},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return v4Shift(args.x.([]int32), args.y.(int))
+			},
+		},
+		{
+			Name: "int32-right-1",
+			Args: args{
+				x: []int32{-1, 1, -2, -3, 1, 5},
+				y: -1,
+			},
+			Want: []int32{1, -2, -3, 1, 5, 0},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return v4Shift(args.x.([]int32), args.y.(int))
+			},
+		},
+		{
+			Name: "float64-vector-v1",
+			Args: args{
+				x: []float64{-1, 1, -2, -3, 1, 5},
+				y: []int{-1, 1, -2, -3, 1, 5},
+			},
+			Want: []float64{1, -1, 1, Float64NaN(), -3, -1},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return v1Shift(args.x.([]float64), args.y.([]int))
+			},
+		},
+		{
+			Name: "int32-vector-v4",
+			Args: args{
+				x: []int32{-1, 1, -2, -3, 1, 5},
+				y: []int32{-1, 1, -2, -3, 1, 5},
+			},
+			Want: []int32{1, -1, 1, Int32NaN, -3, -1},
+			TestFunc: func(v any) any {
+				args := v.(args)
+				return v4Shift(args.x.([]int32), args.y.([]int32))
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			if got := tt.TestFunc(tt.Args); !labs.DeepEqual(got, tt.Want) {
+				t.Errorf("v4Shift() = %v, want %v", got, tt.Want)
+			}
+		})
+	}
+}
+
+func BenchmarkShift_init(b *testing.B) {
+	testalignOnce.Do(initTestData)
+}
+
+func BenchmarkShift_release(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		Shift(f64s, 1)
+	}
+}
+
+func BenchmarkShift_v1(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v1Shift(f64s, 1)
+	}
+}
+
+func BenchmarkShift_v2(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v2Shift(f64s, 1)
+	}
+}
+
+func BenchmarkShift_v3(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v3Shift(f64s, 1)
+	}
+}
+
+func BenchmarkShift_v4(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v4Shift(f64s, 1)
+	}
+}
