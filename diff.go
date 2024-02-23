95aaa9d1122c8b91da2377741893e959c20a8fa8
wangfeng <wangfengxy@sina.cn>
2024-02-23 13:44:10 +0800

    优雅和调整Diff函数

--- diff.go
+++ diff.go
@@ -2,10 +2,17 @@ package num
 
 // Diff 元素的第一个离散差
 //
+//	n默认是1
+func Diff[E Number](S []E, n int) []E {
+	return v4Diff[E](S, n)
+}
+
+// v1Diff 元素的第一个离散差
+//
 //	First discrete difference of element.
 //	Calculates the difference of a {klass} element compared with another
 //	element in the {klass} (default is element in previous row).
-func Diff[T Number](s []T, param any) []T {
+func v1Diff[T Number](s []T, param any) []T {
 	blocks := Rolling[T](s, param)
 	var d []T
 	var front = TypeDefault[T]()
@@ -31,23 +38,28 @@ func Diff[T Number](s []T, param any) []T {
 	return d
 }
 
-func Diff2[T BaseType](s []T, param any) []T {
+// Deprecated: 推荐使用Diff [wangfeng on 2024/2/22 17:51]
+func V2Diff[T BaseType](s []T, param any) []T {
+	return v2Diff[T](s, param)
+}
+
+func v2Diff[T BaseType](s []T, param any) []T {
 	var d any
 	switch vs := any(s).(type) {
 	case []float32:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []float64:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []int:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []int8:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []int16:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []int32:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	case []int64:
-		d = Diff(vs, param)
+		d = v1Diff(vs, param)
 	//case []uint, []uint8, []uint16, []uint32, []uint64, []uintptr:
 	//	d = xv
 	default:
@@ -56,4 +68,77 @@ func Diff2[T BaseType](s []T, param any) []T {
 	}
 
 	return d.([]T)
+}
+
+// 最原始的算法, 只支持向右滑动
+func v3Diff[E Number](S []E, n int) []E {
+	length := len(S)
+	d := make([]E, length)
+	offset := AbsInt(n)
+	defaultValue := TypeDefault[E]()
+	for i, _ := range S[:length-offset] {
+		d[i] = S[i+offset] - S[i]
+	}
+	for i := length - offset; i < length; i++ {
+		d[i] = defaultValue
+	}
+	return d
+}
+
+func v4Diff[E Number](S []E, n int) []E {
+	d := v3Shift(S, n)
+	SubInplace(d, S)
+	return d
+}
+
+// 支持向左向右两个方向滑动
+func v5Diff[E Number](S []E, n int) []E {
+	d := v3Shift(S, n)
+	for i, _ := range S {
+		d[i] -= S[i]
+	}
+	return d
+}
+
+func v6Diff[E Number](S []E, n int) []E {
+	arr := ndarray[E](S)
+	d := arr.v1Rolling(n, func(index int, v E) E {
+		return v - S[index]
+	})
+	return d
+}
+
+func v7Diff[E Number](S []E, n int) []E {
+	arr := ndarray[E](S)
+	d := arr.v2Rolling(n, func(index int, v E) E {
+		return v - S[index]
+	})
+	return d
+}
+
+func v8Diff[E Number](S []E, n int) []E {
+	length := len(S)
+	d := make([]E, length)
+	window := Any2Window[DType](n)
+	defaultValue := TypeDefault[E]()
+	for i := 0; i < length; i++ {
+		val := S[i]
+		period := window.At(i)
+		if DTypeIsNaN(period) {
+			val = defaultValue
+		} else {
+			newIndex := i - int(period)
+			//if period > 0 {
+			//	newIndex += length
+			//}
+			//newIndex = newIndex % length
+			if newIndex < 0 || newIndex >= length {
+				val = defaultValue
+			} else {
+				val = S[newIndex] - val
+			}
+		}
+		d[i] = val
+	}
+	return d
 }
--- diff_test.go
+++ diff_test.go
@@ -2,26 +2,281 @@ package num
 
 import (
 	"fmt"
+	"gitee.com/quant1x/num/labs"
+	"slices"
 	"testing"
 )
 
-func TestDiff(t *testing.T) {
+func TestDiff1(t *testing.T) {
 	d1 := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	fmt.Println(d1)
 	fmt.Println("------------------------------------------------------------")
 	N := 1
 	fmt.Println("固定的参数, N =", N)
-	r1 := Diff(d1, N)
+	r1 := v1Diff(d1, N)
 	fmt.Println("序列化结果:", r1)
 	fmt.Println("------------------------------------------------------------")
 	s1 := []float64{1, 2, 3, 4, 3, 3, 2, 1, Nil2Float64, Nil2Float64, Nil2Float64, Nil2Float64}
 	fmt.Printf("序列化参数: %+v\n", s1)
-	r2 := Diff(d1, s1)
+	r2 := v1Diff(d1, s1)
 	fmt.Println("序列化结果:", r2)
 }
 
 func TestDiff2(t *testing.T) {
 	d1 := []float64{1, 2, 3, 4, 5}
-	r1 := Diff2(d1, 1)
+	r1 := v2Diff(d1, 1)
 	fmt.Println(r1)
+}
+
+func BenchmarkDiff_init(b *testing.B) {
+	testalignOnce.Do(initTestData)
+}
+
+func BenchmarkDiff_release(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v1Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v1(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v1Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v2(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v2Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v3(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v3Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v4(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	useAvx2 := GetAvx2Enabled()
+	SetAvx2Enabled(true)
+	defer SetAvx2Enabled(useAvx2)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v4Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v5(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v5Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v6(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v6Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v7(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v7Diff(f64s, 1)
+	}
+}
+
+func BenchmarkDiff_v8(b *testing.B) {
+	testalignOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	for i := 0; i < b.N; i++ {
+		v8Diff(f64s, 1)
+	}
+}
+
+func Test_v1Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v3",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: 1,
+			},
+			want: []float64{Float64NaN(), 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v1Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v1Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_v2Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v3",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: 1,
+			},
+			want: []float64{Float64NaN(), 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v2Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v2Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_v3Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v3",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: 1,
+			},
+			want: []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, Float64NaN()},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v3Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v3Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_v4Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v4",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: -1,
+			},
+			want: []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, Float64NaN()},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v4Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v4Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_v5Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v5",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: 1,
+			},
+			want: []float64{Float64NaN(), -1, -1, -1, -1, -1, -1, -1, -1, -1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v5Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v5Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_v6Diff(t *testing.T) {
+	type args[E Number] struct {
+		S []E
+		n int
+	}
+	type testCase[E Number] struct {
+		name string
+		args args[E]
+		want []E
+	}
+	tests := []testCase[float64]{
+		{
+			name: "v6",
+			args: args[float64]{
+				S: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				n: 1,
+			},
+			want: []float64{Float64NaN(), -1, -1, -1, -1, -1, -1, -1, -1, -1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v6Diff(tt.args.S, tt.args.n); !labs.DeepEqual(got, tt.want) {
+				t.Errorf("v6Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
