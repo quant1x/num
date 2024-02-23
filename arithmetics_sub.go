5f79af83bc80f1e16d05f005a5ffef212a11ca64
wangfeng <wangfengxy@sina.cn>
2024-02-23 10:22:39 +0800

    新增inplace方式的sub函数 x = x - y

--- arithmetics_sub.go
+++ arithmetics_sub.go
@@ -6,15 +6,42 @@ import (
 	"slices"
 )
 
-// Sub arithmetics 减法
+// Sub arithmetics 减法 z = x - y
 func Sub[T Number](x []T, y any) []T {
-	return BinaryOperations(x, y, x32.Sub, x64.Sub, __sub_go[T])
+	return BinaryOperations(x, y, x32.Sub, x64.Sub, __go_sub[T])
 }
 
-func __sub_go[T Number](x, y []T) []T {
-	x = slices.Clone(x)
+func __go_sub[T Number](x, y []T) []T {
+	d := slices.Clone(x)
+	for i := 0; i < len(x); i++ {
+		d[i] -= y[i]
+	}
+	return d
+}
+
+// SubInplace 减法 x = x - y
+func SubInplace[T Number](x []T, y any) []T {
+	var d any
+	length := len(x)
+	switch vs := any(x).(type) {
+	case []float32:
+		f32s := AnyToSlice[float32](y, length)
+		x32.Sub_Inplace(vs, f32s)
+		d = vs
+	case []float64:
+		f64s := AnyToSlice[float64](y, length)
+		x64.Sub_Inplace(vs, f64s)
+		d = vs
+	default:
+		ys := AnyToSlice[T](y, length)
+		__go_sub_inplace[T](x, ys)
+		d = x
+	}
+	return d.([]T)
+}
+
+func __go_sub_inplace[T Number](x, y []T) {
 	for i := 0; i < len(x); i++ {
 		x[i] -= y[i]
 	}
-	return x
 }
