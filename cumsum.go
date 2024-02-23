54606a39d5d3677d7145d703def3107049a0fbb2
wangfeng <wangfengxy@sina.cn>
2024-02-23 15:41:43 +0800

    调整cumsum函数

--- cumsum.go
+++ cumsum.go
@@ -8,15 +8,15 @@ import (
 
 // CumSum 计算累和
 func CumSum[T Number](x []T) []T {
-	return UnaryOperations(x, x32.CumSum, x64.CumSum, __cumsum_go[T])
+	return UnaryOperations(x, x32.CumSum, x64.CumSum, __go_cumsum[T])
 }
 
-func __cumsum_go[T Number](x []T) []T {
-	x = slices.Clone(x)
+func __go_cumsum[T Number](x []T) []T {
+	d := slices.Clone(x)
 	sum := T(0)
-	for i := 0; i < len(x); i++ {
-		sum += x[i]
-		x[i] = sum
+	for i := 0; i < len(d); i++ {
+		sum += d[i]
+		d[i] = sum
 	}
-	return x
+	return d
 }
