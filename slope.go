54f395790d3b5b9668bb45a60548340692f88ce5
wangfeng <wangfengxy@sina.cn>
2024-02-24 20:00:45 +0800

    优化平行线的计算方法

--- slope.go
+++ slope.go
@@ -1,5 +1,14 @@
 package num
 
+import "math"
+
+const (
+	// 1度=多少弧度
+	radiansPreDegrees = math.Pi / 180.00
+	// 1弧度=多少度
+	degreesPreRadian = 180.00 / math.Pi
+)
+
 // Slope 计算斜率
 func Slope(x1 int, y1 float64, x2 int, y2 float64) float64 {
 	return (y2 - y1) / float64(x2-x1)
@@ -8,4 +17,121 @@ func Slope(x1 int, y1 float64, x2 int, y2 float64) float64 {
 // TriangleBevel 三角形斜边
 func TriangleBevel(slope float64, x1 int, y1 float64, xn int) float64 {
 	return slope*float64(xn-x1) + y1
+}
+
+// degreesToRadian 角度转弧度
+//
+//	弧度=角度÷180×π
+func degreesToRadian(degrees float64) float64 {
+	//return degrees * math.Pi / 180
+	return degrees * radiansPreDegrees
+}
+
+// radianToDegrees 弧度转角度
+//
+//	角度=弧度×180÷π
+func radianToDegrees(radian float64) float64 {
+	//return (radian * 180) / math.Pi
+	return radian * degreesPreRadian
+}
+
+// DegreesToSlope 角度转斜率
+func DegreesToSlope(x float64) float64 {
+	radians := degreesToRadian(x)
+	return math.Tan(radians)
+}
+
+// SlopeToDegrees 斜率转角度
+func SlopeToDegrees(m float64) float64 {
+	//return math.Atan(m) * 180 / math.Pi
+	radians := math.Atan(m)
+	degrees := radianToDegrees(radians)
+	return degrees
+}
+
+// Point 点
+type Point struct {
+	X float64 // x 轴
+	Y float64 // y 轴
+}
+
+// Line LineEquation 线性方程式
+//
+//	① Ax + By + C = 0
+//	② 斜率k = -(A/B)
+//	③ x截距 = -(C/A), y截距 = -(C/B)
+type Line struct {
+	slope     float64 // 斜率, k
+	intercept float64 // 截距, b
+}
+
+// CalculateLineEquation 已知两个点, 计算线性方程式
+func CalculateLineEquation(point1, point2 Point) Line {
+	m := (point2.Y - point1.Y) / (point2.X - point1.X)
+	c := point1.Y - m*point1.X
+
+	line := Line{slope: m, intercept: c}
+	return line
+}
+
+func (this Line) Radian() float64 {
+	return math.Atan(this.slope)
+}
+
+// Degrees 计算角度
+func (this Line) Degrees() float64 {
+	return math.Atan(this.slope) * 180 / math.Pi
+}
+
+// Equation 返回方程式 A, B, C
+func (this Line) Equation() (A, B, C float64) {
+	A = -this.slope
+	B = 1.0
+	C = -this.intercept
+	return
+}
+
+// Distance 点到线的距离
+//
+//	                         ___________
+//	公式 |(Ax0 + By0 + C)| / √(A^2 + B^2)
+func (this Line) Distance(p Point) float64 {
+	A, B, C := this.Equation()
+	numerator := math.Abs(A*p.X + B*p.Y + C)
+	denominator := math.Sqrt(A*A + B*B)
+	distance := numerator / denominator
+	return distance
+}
+
+// ParallelLine 通过 一个点的x轴坐标计算一个新的平行线
+func (this Line) ParallelLine(x float64) Line {
+	intercept := this.intercept - this.slope*x
+	return Line{slope: this.slope, intercept: intercept}
+}
+
+//// 计算 斜边
+//func (this Line)Hypotenuse(p Point) float64 {
+//
+//}
+
+// SymmetricParallelLine 计算 点对称(等距离)的平行线
+func (this Line) SymmetricParallelLine(p Point) Line {
+	// 1. 确定点p到line的距离
+	distance := this.Distance(p)
+	// 2. 规划直角三角形
+	// 2.1 斜边 Hypotenuse
+	hypotenuse := 2 * distance
+	// 2.2 计算 斜边于底边的角度
+	lineDegrees := this.Degrees()
+	d1 := 180 - lineDegrees
+	degrees := 90 - d1
+	radian := degreesToRadian(degrees)
+	// 2.3 底边 Opposite side
+	opposite := hypotenuse * math.Cos(radian)
+	// 2.4 邻边 Adjacent side
+	adjacent := hypotenuse * math.Sin(radian)
+	// 3. 计算新的平行线
+	line := this.ParallelLine(opposite)
+	_ = adjacent
+	return line
 }
--- slope_test.go
+++ slope_test.go
@@ -1,9 +1,37 @@
 package num
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
+
+func TestLine_basic(t *testing.T) {
+	degrees := 90.00
+	slope := DegreesToSlope(degrees)
+	fmt.Println(slope)
+	degrees = SlopeToDegrees(slope)
+	fmt.Println(degrees)
+}
+
+func TestLineEquation(t *testing.T) {
+	p1 := Point{0, 3}
+	p2 := Point{4, 0}
+
+	line := CalculateLineEquation(p2, p1)
+	fmt.Println(line)
+	r1 := line.Radian()
+	d1 := radianToDegrees(r1)
+	d2 := 90 - math.Abs(d1)
+	r2 := degreesToRadian(d2)
+	b := p1.Y/math.Sin(math.Abs(r1)) == p2.X/math.Sin(r2)
+	fmt.Println(b)
+	d3 := 90.00
+	r3 := degreesToRadian(d3)
+	radio := p1.Y / math.Sin(math.Abs(r1))
+	hypotenuse := radio * math.Sin(r3)
+	fmt.Println(hypotenuse)
+}
 
 func TestSlope(t *testing.T) {
 	a := math.Nextafter(2.0, 3.0)
@@ -19,4 +47,148 @@ func TestSlope(t *testing.T) {
 	x3 := 6
 	y3 := xl*float64(x3-x2) + y2
 	t.Logf("y3 = %f", y3)
+}
+
+func TestLine_Degrees(t *testing.T) {
+	type fields struct {
+		slope     float64
+		intercept float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		{
+			name:   "45°",
+			fields: fields{slope: 1, intercept: 1},
+			want:   45,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := Line{
+				slope:     tt.fields.slope,
+				intercept: tt.fields.intercept,
+			}
+			if got := this.Degrees(); got != tt.want {
+				t.Errorf("Degrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// 计算点 (x0, y0) 到直线 Ax + By + C = 0 的距离
+func distancePointLine(A, B, C float64, x0, y0 float64) float64 {
+	return math.Abs(A*x0+B*y0+C) / math.Sqrt(A*A+B*B)
+}
+
+func TestLine_Distance_basic(t *testing.T) {
+	A, B, C := 1.0, -1.0, 2.0 // 直线的系数，例如 A=1, B=-1, C=2 对应于方程 x - y + 2 = 0
+	x0, y0 := 3.0, 4.0        // 点的坐标
+	d := distancePointLine(A, B, C, x0, y0)
+	fmt.Printf("The distance from point (%.2f, %.2f) to the line is %.2f\n", x0, y0, d)
+}
+
+func TestLine_Distance(t *testing.T) {
+	type fields struct {
+		slope     float64
+		intercept float64
+	}
+	type args struct {
+		p Point
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   float64
+	}{
+		{
+			name:   "d1",
+			fields: fields{slope: -1, intercept: -2},
+			args:   args{p: Point{4, 5}},
+			want:   7.7781745930520225,
+		},
+		{
+			name:   "d2",
+			fields: fields{slope: 2, intercept: 3},
+			args:   args{p: Point{4, 5}},
+			want:   2.6832815729997477,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := Line{
+				slope:     tt.fields.slope,
+				intercept: tt.fields.intercept,
+			}
+			if got := this.Distance(tt.args.p); got != tt.want {
+				t.Errorf("Distance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSlopeToDegrees(t *testing.T) {
+	type args struct {
+		m float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "t1",
+			args: args{m: 0.5},
+			want: 26.56505117707799,
+		},
+		{
+			name: "t2",
+			args: args{m: 1},
+			want: 45,
+		},
+		{
+			name: "t3",
+			args: args{m: 0},
+			want: 0.0,
+		},
+		{
+			name: "t4",
+			args: args{m: 1.6331239353195392e+16},
+			want: 90,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SlopeToDegrees(tt.args.m); got != tt.want {
+				t.Errorf("SlopeToDegrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDegreesToSlope(t *testing.T) {
+	type args struct {
+		x float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "t1",
+			args: args{x: 45},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DegreesToSlope(tt.args.x); got != tt.want {
+				t.Errorf("DegreesToSlope() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
