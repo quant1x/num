1498ff61e18831f025b0bb44bf228f9b69613082
wangfeng <wangfengxy@sina.cn>
2024-02-26 08:31:25 +0800

    新增周期结构体, 只支持DType类型

---
+++ period.go
@@ -0,0 +1,84 @@
+package num
+
+// Period 周期
+type Period struct {
+	Array []DType
+	N     DType
+}
+
+// At 获取下标为i的元素
+// 如果i超过切片V的长度, 则直接返回常量C
+func (this Period) At(i int) DType {
+	if i < len(this.Array) {
+		return this.Array[i]
+	}
+	return this.N
+}
+
+// IsConst 是否全部常量
+func (this Period) IsConst() bool {
+	return len(this.Array) == 0 && this.N != 0
+}
+
+func AnyToPeriod(n any) Period {
+	var constant DType
+	var array []DType
+	switch m := n.(type) {
+	case float32:
+		constant = DType(m)
+	case float64:
+		constant = DType(m)
+	case int8:
+		constant = DType(m)
+	case uint8:
+		constant = DType(m)
+	case int16:
+		constant = DType(m)
+	case uint16:
+		constant = DType(m)
+	case int32:
+		constant = DType(m)
+	case uint32:
+		constant = DType(m)
+	case int64:
+		constant = DType(m)
+	case uint64:
+		constant = DType(m)
+	case int:
+		constant = DType(m)
+	case uint:
+		constant = DType(m)
+	case uintptr:
+		constant = DType(m)
+	case []float32:
+		array = AnyToSlice[DType](m, len(m))
+	case []float64:
+		array = AnyToSlice[DType](m, len(m))
+	case []int8:
+		array = AnyToSlice[DType](m, len(m))
+	case []uint8:
+		array = AnyToSlice[DType](m, len(m))
+	case []int16:
+		array = AnyToSlice[DType](m, len(m))
+	case []uint16:
+		array = AnyToSlice[DType](m, len(m))
+	case []int32:
+		array = AnyToSlice[DType](m, len(m))
+	case []uint32:
+		array = AnyToSlice[DType](m, len(m))
+	case []int64:
+		array = AnyToSlice[DType](m, len(m))
+	case []uint64:
+		array = AnyToSlice[DType](m, len(m))
+	case []int:
+		array = AnyToSlice[DType](m, len(m))
+	case []uint:
+		array = AnyToSlice[DType](m, len(m))
+	case []uintptr:
+		array = AnyToSlice[DType](m, len(m))
+	case DTypeArray:
+		tmp := m.DTypes()
+		array = AnyToSlice[DType](tmp, len(tmp))
+	}
+	return Period{Array: array, N: constant}
+}
