2e588b601b03c859c28c4398b492e3c2663ef73a
wangfeng <wangfengxy@sina.cn>
2024-02-26 10:23:00 +0800

    优化window的类型匹配

--- window.go
+++ window.go
@@ -1,6 +1,8 @@
 package num
 
 // Window 参数N的新方式
+//
+// Deprecated: 推荐 Periods [wangfeng on 2024/2/26 09:50]
 type Window[E Number] struct {
 	V []E // 切片
 	C E   // 常量
@@ -10,25 +12,62 @@ func Any2Window[E Number](n any) Window[E] {
 	var constant E
 	var array []E
 	switch m := n.(type) {
+	case Periods:
+		return Window[E]{
+			V: AnyToSlice[E](m.Array, len(m.Array)),
+			C: E(m.N),
+		}
 	case float32:
 		constant = E(m)
 	case float64:
 		constant = E(m)
+	case int8:
+		constant = E(m)
+	case uint8:
+		constant = E(m)
+	case int16:
+		constant = E(m)
+	case uint16:
+		constant = E(m)
 	case int32:
+		constant = E(m)
+	case uint32:
 		constant = E(m)
 	case int64:
 		constant = E(m)
+	case uint64:
+		constant = E(m)
 	case int:
+		constant = E(m)
+	case uint:
+		constant = E(m)
+	case uintptr:
 		constant = E(m)
 	case []float32:
 		array = AnyToSlice[E](m, len(m))
 	case []float64:
 		array = AnyToSlice[E](m, len(m))
+	case []int8:
+		array = AnyToSlice[E](m, len(m))
+	case []uint8:
+		array = AnyToSlice[E](m, len(m))
+	case []int16:
+		array = AnyToSlice[E](m, len(m))
+	case []uint16:
+		array = AnyToSlice[E](m, len(m))
 	case []int32:
+		array = AnyToSlice[E](m, len(m))
+	case []uint32:
 		array = AnyToSlice[E](m, len(m))
 	case []int64:
 		array = AnyToSlice[E](m, len(m))
+	case []uint64:
+		array = AnyToSlice[E](m, len(m))
 	case []int:
+		array = AnyToSlice[E](m, len(m))
+	case []uint:
+		array = AnyToSlice[E](m, len(m))
+	case []uintptr:
 		array = AnyToSlice[E](m, len(m))
 	case DTypeArray:
 		tmp := m.DTypes()
