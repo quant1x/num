06cabb6b12a86293cde2e0fa0bd96cc4e6c32447
wangfeng <wangfengxy@sina.cn>
2024-03-10 11:27:48 +0800

    新增cross方法, 返回趋势改变的状态切片

---
+++ cross.go
@@ -0,0 +1,35 @@
+package num
+
+// 趋势变化
+const (
+	Unchanged       = 0  // 趋势不变
+	BreakThrough    = 1  // break through 突破
+	FallDrastically = -1 // fall drastically 跌破
+)
+
+type PointShape struct {
+	X     int // 索引
+	State int // 状态
+}
+
+// Cross 上穿和下穿
+func Cross[E Number](a, b []E) []PointShape {
+	length := len(a)
+	list := make([]PointShape, length)
+	count := 0
+	for i := 1; i < length; i++ {
+		front := i - 1
+		current := i
+		if a[front] < b[front] && a[current] > b[current] {
+			// a 上穿 b
+			list[count] = PointShape{X: current, State: BreakThrough}
+			count++
+		} else if a[front] < b[front] && a[current] > b[current] {
+			// a 下穿 b
+			list[count] = PointShape{X: current, State: FallDrastically}
+			count++
+		}
+	}
+	list = list[:count]
+	return list
+}
